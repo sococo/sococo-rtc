bb2267415c4bc062a9ccba0414fadaca1bef36aa
Justin DuJardin <justin.dujardin@sococo.com>
2014-03-04 11:06:01 -0800

    Add basic skeleton server with express/Faye and static file serving.

---
+++ server.js
@@ -0,0 +1,42 @@
+/*jslint node:true */
+"use strict";
+var path = require('path');
+var express = require('express');
+var server = express();
+var faye = require('faye');
+var bayeux = new faye.NodeAdapter({
+   mount:    '/meet',
+   timeout:  45
+});
+var env = server.get('env');
+
+var serverPort = 4202;
+
+// gzip scripts/css when possible.
+server.use(express.compress());
+
+// Pretty print HTML outputs in development and debug configurations
+if(env !== 'production'){
+   server.locals.pretty = true;
+}
+
+// Development Settings
+server.configure(function(){
+   server.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+   }));
+});
+// Production Settings
+server.configure('production', function(){
+   server.use(express.errorHandler());
+});
+
+
+// Mount the `public` directory for static file serving.
+server.use(express.static(path.resolve(__dirname + "/public")));
+
+// Set up faye realtime connections
+var hServer = server.listen(serverPort);
+bayeux.attach(hServer);
+console.log("Server running on port " + serverPort);
