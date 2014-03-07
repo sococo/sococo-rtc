62e87ccc63ce0c0bea6b1f1ef12fae643f38b706
Justin DuJardin <justin.dujardin@sococo.com>
2014-03-06 17:41:58 -0800

    Have to server render app page to embed the proper websocket server address.

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ var path = require('path');
 var express = require('express');
 var server = express();
 var faye = require('faye');
+var bayeuxMount = "/meet";
 var bayeux = new faye.NodeAdapter({
-   mount:    '/meet',
+   mount:    bayeuxMount,
    timeout:  45
 });
 var env = server.get('env');
@@ -32,6 +33,28 @@ server.configure(function(){
 server.configure('production', function(){
    server.use(express.errorHandler());
 });
+
+
+server.get('/', function(req,res){
+   // Attempt to get IP from heroku instance
+   // From: http://lostechies.com/derickbailey/2013/12/04/getting-the-real-client-ip-address-on-a-heroku-hosted-nodejs-app/
+   var wsHost = req.headers["x-forwarded-for"];
+   if (wsHost){
+      var list = wsHost.split(",");
+      wsHost = list[list.length-1];
+   } else {
+      wsHost = req.connection.remoteAddress;
+   }
+   res.render('index.html',{
+      pubsub:wsHost + ":" + serverPort,
+      pubsubmount:bayeuxMount
+   });
+});
+
+// Use EJS templating with Express, and assign .html as the default extension.
+server.engine('.html', require('ejs').__express);
+server.set('view engine', 'html');
+server.set('views',"public/");
 
 
 // Mount the `public` directory for static file serving.
