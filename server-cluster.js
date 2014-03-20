4b2539d611e9c855d6616ceb21af59d1fc354a37
Justin DuJardin <justin.dujardin@sococo.com>
2014-03-20 14:02:54 -0700

    Respawn dead workers.

--- server-cluster.js
+++ server-cluster.js
@@ -1,9 +1,16 @@
 "use strict";
 var cluster = require('cluster');
 if(cluster.isMaster){
-   // TODO: Configure based on heroku dynos?  Get CPU count?
-   for(var i = 0; i < 4; i++){
-      var worker = cluster.fork();
+   var workerCount = process.env.WORKER_COUNT || require('os').cpus().length;
+   var workers = new Array(workerCount);
+   for(var i = 0; i < workerCount; i++){
+      !function spawn(i) {
+         workers[i] = cluster.fork();
+         workers[i].on('exit', function() {
+            console.error('sococo-rtc: worker died, respawning');
+            spawn(i);
+         });
+      }(i);
    }
 }
 else {
